2acf6fa06f51bbd92512e46d64c40f305e8fe30f
Daniel Thorp <arcanistzed@gmail.com>
2024-02-15 06:33:19 +0000

    feat: initial implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import uosu from "./assets/uosu.svg";
+import ge from "./assets/ge.svg";
+import "./App.css";
+
+const CORS = "https://cors.arcanist.workers.dev/";
+const URL = "https://secure.electionbuddy.com/widget/JRL2D6SJT62K.js";
+const APPENDIX = `window.r = r;`;
+
+type ElectionData = {
+	election_name: string;
+	spoiled_ballot_count: number;
+	submitted_ballot_count: number;
+	pluralized_submitted_ballot_count: string;
+	pluralized_eligible_voter_count: string;
+	ballot_count_percentage: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [data, setData] = useState<ElectionData | null>(null);
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await fetch(`${CORS}${URL}`);
+			const wrapped = await response.text();
+			const script = wrapped.slice(2, -2).split("\n")[0];
+			new Function(`${script}${APPENDIX}`)();
+			if ("r" in window) {
+				const r = window.r as ElectionData;
+				r.pluralized_submitted_ballot_count = r.pluralized_submitted_ballot_count.replace(" ballots", "");
+				r.pluralized_eligible_voter_count = r.pluralized_eligible_voter_count.replace(" eligible voters", "");
+				setData(r);
+			} else {
+				console.error("Failed to load script");
+			}
+		};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+		fetchData();
+	});
+
+	return (
+		<>
+			<div
+				className="blob"
+				style={{
+					backgroundColor: "#FFC268",
+					width: "60vmin",
+					top: "-10vmin",
+					left: "-5vmin",
+				}}
+			></div>
+			<div
+				className="blob"
+				style={{
+					backgroundColor: "#7380D8",
+					width: "60vmin",
+					right: "-20vmin",
+					top: "20vmin",
+				}}
+			></div>
+			<div
+				className="blob"
+				style={{
+					backgroundColor: "#73B4D8",
+					width: "45vmin",
+					bottom: "-5vmin",
+					left: "-5vmin",
+				}}
+			></div>
+			{data ? (
+				<>
+					<img src={ge} alt="Élections générales 2024 General Elections" />
+					<div className="group">
+						<h1>Participation électorale</h1>
+						<h1>Voter turnout</h1>
+					</div>
+					<div className="group">
+						<p>{data.ballot_count_percentage}</p>
+						<small>
+							{data.pluralized_submitted_ballot_count} / {data.pluralized_eligible_voter_count}
+						</small>
+					</div>
+					<img
+						src={uosu}
+						width={400}
+						alt="Syndicat étudiant de l'Université d'Ottawa | University of Ottawa Students' Union"
+					/>
+				</>
+			) : (
+				<p>Loading...</p>
+			)}
+		</>
+	);
 }
 
-export default App
+export default App;
